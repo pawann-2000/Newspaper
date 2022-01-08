bf8956e80827aed2faa343fa21511de3e0ce57fe
pawann-2000 <59572141+pawann-2000@users.noreply.github.com>
2022-01-08 18:17:09 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-[![Run on Repl.it](https://repl.it/badge/github/pawan-py/Default)](https://repl.it/github/pawan-py/Default)
+# A Collaborative and Open source News bot
+
+## Haven't yet decided a name to give but let me tell you what this bot does:
+
+###-> Provides news for developers.
+###-> Provides news for any source you want (to be a feature in future).
+###-> And many more features to be implemented in future.
---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event 
+async def on_message(message):
+    if 'ping' in message.content.lower():
+        await message.channel.send('Ping successful')
+    if 'Hi' in message.content():
+        await message.channel.send('Hello')
+
+client.run('TOKEN')
+

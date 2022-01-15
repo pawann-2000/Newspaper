d51e5e444a9db526959ccb0fcb3a94bdfdd3f20d
pawann-2000 <59572141+pawann-2000@users.noreply.github.com>
2022-01-15 14:02:46 +0530

    minor changes

--- grabber.py
+++ grabber.py
@@ -1,29 +1,39 @@
-#imported some web scraping libs here
+# imported some web scraping libs here
 import requests
 from bs4 import BeautifulSoup
 import pprint
 
-#vars
+# vars
 response = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(response.text, 'html.parser')
 links = soup.select('.titlelink')
 subtext = soup.select('.subtext')
 
-#Testing out this function still, give it a run to see what it does
+# Testing out this function still, give it a run to see what it does
+
+
 def grabber(links, subtext):
     hn = []
-    for dex, item in enumerate(links):
+
+    for index, item in enumerate(links):
+
         title = item.getText()
         href = item.get('href', None)
-        vote = subtext[dex].select('.score')
+        vote = subtext[index].select('.score')
+
         if len(vote):
             votes = int(vote[0].getText().replace(' points', ''))
-            if votes>99:
+
+            if votes > 99:
                 hn.append({'title': title, 'link': href, 'votes': votes})
+
     return sort_stories_by_votes(hn)
 
-#Sorting function to sort the votes in descending order
+# Sorting function to sort the votes in descending order
+
+
 def sort_stories_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
 
 pprint.pprint(grabber(links, subtext))
